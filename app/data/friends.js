89c859df887fbfbe63e94075c3bbc97567448470
karithomas <kari.k.thomas8@gmail.com>
2020-01-28 15:56:32 -0500

    updating file structure

---
+++ app/data/friends.js
@@ -0,0 +1,132 @@
+var friends = [
+    {
+        name: "Harry Potter",
+        photo:"https://gomerblog.com/wp-content/uploads/2018/05/Emergency-Medicine.jpg",
+        scores: [
+            "4", 
+            "1",
+            "3",
+            "1",
+            "4",
+            "4",
+            "5",
+            "2",
+            "4",
+            "3",
+        ]
+    },
+    {
+        name: "Hermione Granger",
+        photo:"https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Hermione_Granger_poster.jpg/220px-Hermione_Granger_poster.jpg",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "1",
+            "4",
+            "4",
+            "5",
+            "2",
+            "4",
+            "5",
+        ]
+    },
+    {
+        name: "Draco Malfoy",
+        photo:"https://images.ctfassets.net/usf1vwtuqyxm/14uf2d7MCaIkeC28q2IMaW/6bac8ad26385eb146124e8b77513ec87/DracoMalfoy_WB_F6_EmotionalDracoPointingWand_Promo_080615_Port.jpg",
+        scores: [
+            "4",
+            "1",
+            "5",
+            "1",
+            "5",
+            "2",
+            "1",
+            "5",
+            "1",
+            "3",
+        ] 
+    },
+    {
+        name: "Ron Weasley",
+        photo:"https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Ron_Weasley_poster.jpg/220px-Ron_Weasley_poster.jpg",
+        scores: [
+            "3",
+            "5",
+            "5",
+            "1",
+            "3",
+            "4",
+            "5",
+            "2",
+            "3",
+            "2",
+        ]  
+    },
+    {
+        name: "Luna Lovegood",
+        photo:"hhttps://images.ctfassets.net/usf1vwtuqyxm/Mam68Vfou2OO6kqEcyW8W/41657e4dbb7d42d2cab591276105bcc1/LunaLovegood_WB_F6_LunaLovegoodInQuibblerSpecsOnHogwartsExpress_Still_080615_Port.jpg",
+        scores: [
+            "4",
+            "2",
+            "1",
+            "3",
+            "3",
+            "4",
+            "5",
+            "2",
+            "3",
+            "4",
+        ]  
+    },
+    {
+        name: "Neville Longbottom",
+        photo:"https://vignette.wikia.nocookie.net/harry-potter/images/6/6c/Neville_Longbottom_portrait_1.jpg/revision/latest/top-crop/width/360/height/450?cb=20090801204451",
+        scores: [
+            "4",
+            "4",
+            "3",
+            "1",
+            "2",
+            "3",
+            "5",
+            "2",
+            "3",
+            "4",
+        ] 
+    },
+    {
+        name: "Remus Lupin",
+        photo:"https://images.ctfassets.net/usf1vwtuqyxm/1PmbUawkeAO6i4ayKiuU4c/1456f90472ea320a61b451812a647051/RemusLupin_WB_F6_RemusLupinStandingWithWand_Promo_080615_Port.jpg",
+        scores: [
+            "5",
+            "1",
+            "1",
+            "5",
+            "5",
+            "2",
+            "5",
+            "2",
+            "4",
+            "5",
+        ]   
+    },
+    {
+        name: "Nymphadora Tonks",
+        photo:"https://images.ctfassets.net/usf1vwtuqyxm/4RCmWhTVFSUk4CQGuYqIIm/e947cbf6cbae0ece4acc39622c6e913f/NymphadoraTonks_WB_F5_NympadoraTonksPromoShot_Promo_100615_Port.jpg",
+        scores: [
+            "4",
+            "1",
+            "4",
+            "5",
+            "2",
+            "3",
+            "5",
+            "3",
+            "4",
+            "3",
+        ]     
+    }
+];
+
+module.exports = friends;
---
+++ app/routing/ApiRoutes.js
@@ -0,0 +1,61 @@
+var friends = require("../data/friends");
+
+module.exports = function(app) {
+  https://cors-anywhere.herokuapp.com/
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+  app.post("/api/friends", function(req, res) {
+    var totalDifference = 0;
+    var bestMatch = {
+      name: " ",
+      photo: " ",
+      friendDifference: 1000
+    };
+
+    var userData = req.body;
+    var userName = userData.name;
+    var userScores = userData.scores;
+
+    var b = userScores.map(function(item){
+      return parseInt(item, 10);
+    });
+    userData = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: b
+    };
+
+    console.log("Name" + userName);
+    console.log("User Scores" + userScores);
+
+    var sum = b.reduce((a, b ) => a+ b, 0);
+    console.log("some of users scores " + sum);
+    console.log("Best match friend difference " + bestMatch.friendDifference);
+    console.log("=========================");
+
+    for (var i = 0; i < friends.length; i++) {
+      console.log(friends[i].name);
+      totalDifference = 0;
+      console.log("total difference " + totalDifference);
+      console.log("best match friend difference " + bestMatch.friendDifference);
+
+      var bfriendScore = friends[i].scores.reduce((a ,b) => a+ b, 0);
+      console.log("total friend score " + bfriendScore);
+      totalDifference += Math.abs(sum - bfriendScore);
+      console.log("------------------> " + totalDifference);
+
+      if (totalDifference <= bestMatch.friendDifference) {
+        bestMatch.name = friends[i].name;
+        bestMatch.photo = friends[i].photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+      console.log(totalDifference + "total difference");
+    }
+    console.log(bestMatch);
+    friends.push(userData);
+    console.log("new user added");
+    console.log(userData);
+    res.json(bestMatch);
+  });
+};
